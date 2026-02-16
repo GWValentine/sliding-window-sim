8f5828345f2f025c12f975cb9a244d920e32dd98
Grant Valentine <gw_valentine@yahoo.com>
2026-02-15 17:59:41 -0700

    Simple Windows Fix

--- core/windows.py
+++ core/windows.py
@@ -6,7 +6,7 @@ class SendWindow:
 
     def is_blocked(self):
         # If not enough ACKS have been sent back, return true
-        return (self.LFS - self.LAR) > self.size
+        return (self.LFS - self.LAR) >= self.size
     
     # LAR goes based off of most recently received ACK
     def advance_ack(self, ack_id):
@@ -21,4 +21,4 @@ class ReceiveWindow:
 
     def is_blocked(self, incoming_id):
         # If incoming frame is beyond acceptable window
-        return incoming_id > self.LAF
+        return incoming_id <= self.LFR or incoming_id > self.LAF
